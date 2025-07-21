df978581d3efcd21395b2beae595e53f8cf373c1
nardoguy14 <nardoarevalo@me.com>
2025-07-20 23:03:53 -0700

    adding time dimension

--- models/healthcare_demographics.sql
+++ models/healthcare_demographics.sql
@@ -1,5 +1,6 @@
+-- Generate a date for each record spread across the last 90 days
 select
     age,
     gender,
-    current_date as record_date
+    current_date - ((row_number() over (order by age, gender)) % 90)::integer as record_date
 from {{ source('nardosdb', 'healthcare_dataset') }}
