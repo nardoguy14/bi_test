9d8f9fd4b14f2b9cde478b279907517145307bca
nardoguy14 <nardoarevalo@me.com>
2025-07-20 23:45:07 -0700

    adding time dimension

--- models/simple_count.sql
+++ models/simple_count.sql
@@ -1,11 +1,14 @@
 select 
     1 as id,
-    'test' as category
+    'test' as category,
+    current_date as created_date
 union all
 select 
     2 as id,
-    'test' as category
+    'test' as category,
+    current_date as created_date
 union all
 select 
     3 as id,
-    'demo' as category
+    'demo' as category,
+    current_date as created_date
